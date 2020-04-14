f897823a0553ee75fdca1d2c2c451d2f028d4535
Vetin <buffyc9h13n@gmail.com>
2020-04-14 22:25:08 +0300

    add keys prod file for deploy

---
+++ server/keys/keys.prod.js
@@ -0,0 +1,8 @@
+module.exports = {
+  MONGO_URL: process.env.MONGO_URL,
+  JWT: process.env.JWT,
+  ADMIN_ID: process.env.ADMIN_ID,
+  TELEGRAM_CHAT_ID: process.env.TELEGRAM_CHAT_ID,
+  TELEGRAM_BOT_TOKEN: process.env.TELEGRAM_BOT_TOKEN,
+  SITE_URL: process.env.SITE_URL
+};
